d84f0baa4d207179a56270f35e5be3fd65a518c9
Mike Herchel <mike@herchel.com>
2017-06-29 21:21:15 -0400

    Refactoring and delegated events for click events.

--- scripts.js
+++ scripts.js
@@ -1,73 +1,76 @@
 (function() {
+  var keys = [];
+  var sounds = [];
 
-// Get JSON data
-fetch('sounds.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    processData(data);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+  // Get JSON data
+  fetch('sounds.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      processData(data);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 
-  function processData(keys) {
-    // Loop through the object and add drum html
+  function processData(data) {
+    keys = data;
+
+    // Create Drums HTML and audio objects.
     var drumsHTML = '';
+
     for (key in keys) {
-      drumsHTML += '<div class="key key-' + keys[key]['letter'] + '">' +
+      drumsHTML += '<div data-sound="' + keys[key]['wavFile'] + '" class="key key-' + keys[key]['letter'] + '">' +
           '<div class="key-letter">' + keys[key]['letter'] + '</div>' +
           '<label class="key-label">' + keys[key]['label'] + '</label>' +
           '</div>';
-    }
-    document.querySelector('.keys-wrapper').innerHTML = drumsHTML;
 
-    // Sounds array to hold audio objects
-    sounds = [];
-
-    // Loop through again to create audio object and add click event listeners for each drum element
-    for (key in keys) {
-      var drumSelector = document.querySelector('.key-' + keys[key]['letter']);
       var drumSoundFile = 'sounds/' + keys[key]['wavFile'] + '.wav';
       var isAudioLoaded;
-
-      // Create audio objects in sounds array
-      soundKey = keys[key]['wavFile'];
+      var soundKey = keys[key]['wavFile'];
       sounds[soundKey] = new Audio(drumSoundFile);
       sounds[soundKey].oncanplaythrough = isAudioLoaded; // Preload audio
-
-      drumSelector.onclick = (function(drumSound, drumSelector) {
-        return function() {
-          playSound(drumSound, drumSelector);
-        }
-      })(sounds[soundKey], drumSelector);
     }
 
-    // Handle keypress events
-    document.onkeydown = function(e) {
-      if (keys.hasOwnProperty(e.keyCode)) {
-        var drumLetter = keys[e.keyCode]['letter'];
-        var drumLabel = keys[e.keyCode]['label'];
-        var wavFile = keys[e.keyCode]['wavFile'];
-        var drumSound = sounds[wavFile];
-        var drumSelector = document.querySelector('.key-' + drumLetter);
-        playSound(drumSound, drumSelector);
-      }
+    document.querySelector('.keys-wrapper').innerHTML = drumsHTML;
 
-      keyLog = (typeof drumLetter !== 'undefined') ? drumLetter + ' ' + e.keyCode + ' ' + drumLabel : e.keyCode;
-      console.log(keyLog);
-    }
+    document.addEventListener('click', clickEvent);
+    document.addEventListener('keydown', keydownEvent); // @todo move to keypress instead of keydown.
+  }
 
-    // Play the sound and add CSS class to element
-    function playSound(drumSound, drumSelector) {
-      drumSound.currentTime = 0;
-      drumSound.play();
-      drumSelector.classList.add('js-active');
-
-      window.setTimeout(function() {
-        drumSelector.classList.remove('js-active');
-      }, 100);
+  function clickEvent(e) {
+    if (e.target.matches('.key, .key *')) {
+      var drumElement = e.target.getAttribute('data-sound') ? e.target : e.target.parentNode;
+      var sound = sounds[drumElement.getAttribute('data-sound')];
+      playSound(sound, drumElement);
     }
   }
+
+  function keydownEvent(e) {
+    if (keys.hasOwnProperty(e.keyCode)) {
+      var drumLetter = keys[e.keyCode]['letter'];
+      var drumLabel = keys[e.keyCode]['label'];
+      var wavFile = keys[e.keyCode]['wavFile'];
+      var drumSound = sounds[wavFile];
+      var drumElement = document.querySelector('.key-' + drumLetter);
+      playSound(drumSound, drumElement);
+    }
+
+    // Log stuff to the console stuff
+    keyLog = (typeof drumLetter !== 'undefined') ? drumLetter + ' ' + e.keyCode + ' ' + drumLabel : e.keyCode;
+    console.info(keyLog);
+  }
+
+  // Play the sound and add CSS class to element
+  function playSound(drumSound, drumElement) {
+    drumSound.currentTime = 0;
+    drumSound.play();
+    drumElement.classList.add('js-active');
+
+    window.setTimeout(function() {
+      drumElement.classList.remove('js-active');
+    }, 100);
+  }
+
 })();
