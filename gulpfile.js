2c18416d050c62c85b3e657324821941ce21c5ac
Mike Herchel <mike@herchel.com>
2017-07-29 18:07:40 -0400

    Adding uglify to the JS build process.

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,20 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const watch = require('gulp-watch');
+const uglify = require('gulp-uglify');
 
-gulp.task('babel', () =>
+gulp.task('build', () =>
   gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015-ie'],
     }))
     .pipe(concat('_build.js'))
+    .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 );
 
 gulp.task('watch', () => {
-  gulp.watch('src/**/*.js', ['babel']);
+  gulp.watch('src/**/*.js', ['build']);
 });
